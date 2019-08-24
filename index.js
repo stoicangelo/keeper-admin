831c7a1e39a33723a84d092cd289ae1be8126563
stoicangelo <aniketdharcfc@gmail.com>
2019-08-24 15:35:46 +0530

    Initial commit home page running on jade

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname,'public')));
+app.set('view engine', 'jade');
+//app.use(express.static('images'));
+
+app.get('/', function(req, res){
+    //res.sendFile(__dirname+'/public/index.html');
+    res.render('index');
+});
+
+app.get('/insert', function(req, res){
+    res.render('insert');
+    //res.sendFile(__dirname+'/public/insert.html');
+});
+
+app.get('/viewall', function(req, res){
+    //res.sendFile(__dirname+'/public/view-all.html');
+    res.render('view-all');
+});
+
+app.listen(3000, function(){
+    console.log('listening on port 3000');
+});
