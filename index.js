960db1192c6d4e158dd42d31fd19ec14e2fa9a3c
stoicangelo <aniketdharcfc@gmail.com>
2019-08-26 21:41:59 +0530

    insert and viewall pages created

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const mgoose = require('mongoose');
+
+
 
 app.use(express.static(path.join(__dirname,'public')));
 app.set('view engine', 'jade');
