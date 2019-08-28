8d43a16a385bd1b12a684a63737fc98204d0d080
stoicangelo <aniketdharcfc@gmail.com>
2019-08-29 01:39:10 +0530

    update feature + insert dupliction validation

--- index.js
+++ index.js
@@ -42,20 +42,25 @@ app.get('/insert', function(req, res){
 app.post('/insert', function(req, res) {
     var book_title = req.body.ititle;
     Book.findOne({title : book_title}).then(function(result){
-            var book = new Book();
-            book.title = book_title;
-            book.author = req.body.iauthor;
-            book.publishDate = req.body.ipubdate;
-            book.price = req.body.iprice;
-            book.save(function(err) {
-                if(err){
-                    console.log('error occured while inserting Book record : '+err);
-                }
-                else{
-                    console.log('insert hoye geche');
-                    res.redirect('/viewall');
-                }
-            });
+            if(!result){
+                var book = new Book();
+                book.title = book_title;
+                book.author = req.body.iauthor;
+                book.publishDate = req.body.ipubdate;
+                book.price = req.body.iprice;
+                book.save(function(err) {
+                    if(err){
+                        console.log('error occured while inserting Book record : '+err);
+                    }
+                    else{
+                        console.log('insert hoye geche');
+                        res.redirect('/viewall');
+                    }
+                });
+            }
+            else{
+                res.render('insert', {bookExists : true});
+            }
     }).catch(function(err){
         console.log('error is : '+err.toString());
     });
@@ -79,6 +84,38 @@ app.get('/delete', function(req, res){
             console.log('error occured while deleting : '+err);
         }
         res.redirect('/viewall');
+    });
+});
+
+app.get('/detail', function(req, res){
+    var book_title = req.query.title;
+    Book.findOne({title : book_title}).then(function(result){
+        res.render('view-one', {data : result});
+    }).catch(function(err){
+        console.log('Error occured while fetching book info : '+err);
+    });
+});
+
+app.post('/update', function(req, res) {
+    var book_title = req.body.utitle;
+    Book.findOne({title : book_title}).then(function(result){
+        if(result){
+            result.title = book_title;
+            result.author = req.body.uauthor;
+            result.publishDate = req.body.upubdate;
+            result.price = req.body.uprice;
+            result.save(function(err) {
+                if(err){
+                    console.log('error occured while inserting Book record : '+err);
+                }
+                else{
+                    console.log('update hoye geche');
+                    res.redirect('/viewall');
+                }
+            });
+        }
+    }).catch(function(err){
+        console.log('error is : '+err.toString());
     });
 });
 
